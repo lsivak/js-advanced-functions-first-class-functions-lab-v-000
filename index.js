4e4261ff26c3e222e7e219559ee72fb879427168
Lara Sanders <larasanders@gmail.com>
2018-06-07 20:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const returnFirstTwoDrivers = function (drivers) {
   }
 
   const returnLastTwoDrivers = function (drivers) {
-      return [drivers[-1], drivers[-2]]
+      return drivers.slice(-1,-2)
     }
