ee3ae5a837d0c4e241657df110eddd3c193cf650
Lara Sanders <larasanders@gmail.com>
2018-06-07 20:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-(function returnFirstTwoDrivers() {
+function returnFirstTwoDrivers() {
   debugger
   const myFunc = function (drivers) {
     return function () {
       return drivers[0], drivers[1]
     }
   }
-})
+}
