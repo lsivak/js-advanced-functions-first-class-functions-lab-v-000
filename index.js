912009b651234e9d8634878a15a69d790c780993
Lara Sanders <larasanders@gmail.com>
2018-06-07 20:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function returnFirstTwoDrivers(drivers) {
-  debugger
-  const myFunc = function () {
-    return function () {
-      return drivers[0], drivers[1]
-    }
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers[0]
   }
-}
