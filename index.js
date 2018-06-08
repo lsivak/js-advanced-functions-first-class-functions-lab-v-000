22f7345a2cd2cbac1de68884f7d0fcf22c6978b4
Lara Sanders <larasanders@gmail.com>
2018-06-08 17:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const returnFirstTwoDrivers = function (drivers) {
       return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
     }
 const selectingDrivers = [
+  debugger
 returnFirstTwoDrivers(),
 returnLastTwoDrivers()
 ]
