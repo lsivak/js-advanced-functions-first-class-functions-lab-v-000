03902e87b993a094a66b7b38bae1f9b8f26b7feb
Lara Sanders <larasanders@gmail.com>
2018-06-08 17:40:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ const returnFirstTwoDrivers = function (drivers) {
   const returnLastTwoDrivers = function (drivers) {
       return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
     }
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()
-]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
